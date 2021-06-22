034f034e0461b91b1093c505b77032ef707e209a
swetambariandhale <86236984+swetambariandhale@users.noreply.github.com>
2021-06-22 12:19:14 +0530

    Update CountCombinations.java

--- src/main/java/exam/CountCombinations.java
+++ src/main/java/exam/CountCombinations.java
@@ -2,8 +2,34 @@ package exam;
 
 public class CountCombinations {
 
-  public int getNumberOfWays(final int total, final int k) {
-    //TODO write your code here to achieve the desired result as explained in Readme file for this problem.
+  
+static int ways(int n)
+{
+{
+    if (n == 1)
+    {
+        return 1;
+    }
+     
+  
+    if (n == 2)
+    {
+        return 2;
+    }
+    else
+    {
+        return ways(n - 1) + ways(n - 2);
+    }
+}
+ 
+
+public static void main (String[] args)
+{
+    int n = 5;
+     
+    System.out.println("Number of ways = " + ways(n));
+}
+}
     return 0;
   }
 
